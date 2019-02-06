e5d166bd3fbfe414c3b580807869fe514626a3c4
user-name-km <47376659+user-name-km@users.noreply.github.com>
2019-02-06 13:36:26 +0530

    Create krishna.java

---
+++ krishna.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=0;
+		if(a==0)
+		{
+			
+			System.out.println("zero");
+		}
+		else if(a>0)
+		{
+			System.out.println("Positive");
+		}
+			else
+		{
+			System.out.println("Negative");
+		}
+
+}
+}
