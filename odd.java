5643bfa4fd3f4d6e45914b05d0171231e123c84a
user-name-km <47376659+user-name-km@users.noreply.github.com>
2019-02-06 14:29:12 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		Scanner s=new Scanner(System.in);
+		
+		int a;
+		a=s.nextInt();
+		if(a%2==0)
+		{
+			
+			System.out.println("Even");
+		}
+		else if(a<0)
+		{
+			System.out.println("Invalid");
+		}
+			else
+		{
+			System.out.println("Odd");
+		}
+
+}
+}
